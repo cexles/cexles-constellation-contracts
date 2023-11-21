6cea64fcde206d0a3c66d341169898a0259ad3e6
Roman Yarinski <299442676t@gmail.com>
2023-11-21 16:42:18 +0100

    Add: Account contract

--- contracts/Account.sol
+++ contracts/Account.sol
@@ -1,6 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import "./ERC6551Account.sol";
+contract Account {
+    address public transshipment;
+    address public owner;
+    uint256 public state;
 
-contract Account is ERC6551Account {}
+    receive() external payable {}
+
+    function initialize(address _owner, address _transshipment) external {
+        require(_owner != address(0) && _transshipment != address(0) && owner == address(0), "INITIALIZE_FAILED");
+        owner = _owner;
+        transshipment = _transshipment;
+    }
+
+    function execute(
+        address to,
+        uint256 value,
+        bytes calldata data
+    ) external payable virtual returns (bytes memory result) {
+        ++state;
+
+        bool success;
+        (success, result) = to.call{value: value}(data);
+
+        if (!success) {
+            assembly {
+                revert(add(result, 32), mload(result))
+            }
+        }
+    }
+}

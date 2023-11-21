d19c2f3581090e895c8d6f28e6b165703ff79f6d
Roman Yarinski <299442676t@gmail.com>
2023-11-21 16:41:31 +0100

    Remove: EIP6551 contracts

--- contracts/interfaces/ITokenAccess.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity 0.8.19;
-
-import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-
-interface ITokenAccess is IERC721 {
-    function mint(address to, uint256 tokenId) external returns (bool);
-}

86c2865edbf7209f9ac6ed2408da9d108d1ce239
Roman Yarinski <299442676t@gmail.com>
2023-12-10 14:11:15 +0100

    Refactor: add comments fix its style

--- contracts/TransshipmentWorker.sol
+++ contracts/TransshipmentWorker.sol
@@ -8,7 +8,11 @@ import {SafeERC20, IERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openz
 import {ITransshipmentStructures, Client} from "./interfaces/ITransshipmentStructures.sol";
 import {EnumerableMap} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.0/utils/structs/EnumerableMap.sol";
 
-/// @title TransshipmentWorker contract with main logic for Transshipment point contract.
+/**
+ * @title TransshipmentWorker
+ * @dev Abstract contract representing a worker in the Transshipment system.
+ * It includes functionality from CCIPReceiver, OwnerIsCreator, and ITransshipmentStructures.
+ */
 abstract contract TransshipmentWorker is CCIPReceiver, OwnerIsCreator, ITransshipmentStructures {
     using EnumerableMap for EnumerableMap.Bytes32ToUintMap;
     using SafeERC20 for IERC20;
@@ -30,23 +34,29 @@ abstract contract TransshipmentWorker is CCIPReceiver, OwnerIsCreator, ITransshi
 
     IERC20 internal s_linkToken;
 
-    /// @notice Constructor initializes the contract with the router address.
-    /// @param _router The address of the router contract.
-    /// @param _link The address of the link contract.
+    /**
+     *  @notice Constructor initializes the contract with the router address.
+     *  @param _router The address of the router contract.
+     *  @param _link The address of the link contract.
+     */
     constructor(address _router, address _link) CCIPReceiver(_router) {
         s_linkToken = IERC20(_link);
         allowlistedSenders[address(this)] = true;
     }
 
-    /// @dev Modifier to allow only the contract itself to execute a function.
-    /// Throws an exception if called by any account other than the contract itself.
+    /**
+     *  @dev Modifier to allow only the contract itself to execute a function.
+     *  Throws an exception if called by any account other than the contract itself.
+     */
     modifier onlySelf() {
         if (msg.sender != address(this)) revert OnlySelf();
         _;
     }
 
-    /// @dev Modifier that checks if the chain with the given destinationChainSelector is allowlisted.
-    /// @param _destinationChainSelector The selector of the destination chain.
+    /**
+     *  @dev Modifier that checks if the chain with the given destinationChainSelector is allowlisted.
+     *  @param _destinationChainSelector The selector of the destination chain.
+     */
     modifier onlyAllowlistedDestinationChain(uint64 _destinationChainSelector) {
         // Skip for demo
         // if (!allowlistedDestinationChains[_destinationChainSelector])
@@ -54,9 +64,11 @@ abstract contract TransshipmentWorker is CCIPReceiver, OwnerIsCreator, ITransshi
         _;
     }
 
-    /// @dev Modifier that checks if the chain with the given sourceChainSelector is allowlisted and if the sender is allowlisted.
-    /// @param _sourceChainSelector The selector of the destination chain.
-    /// @param _sender The address of the sender.
+    /**
+     *  @dev Modifier that checks if the chain with the given sourceChainSelector is allowlisted and if the sender is allowlisted.
+     *  @param _sourceChainSelector The selector of the destination chain.
+     *  @param _sender The address of the sender.
+     */
     modifier onlyAllowlisted(uint64 _sourceChainSelector, address _sender) {
         // Skip for demo
         // if (!allowlistedSourceChains[_sourceChainSelector]) revert SourceChainNotAllowed(_sourceChainSelector);
@@ -64,43 +76,53 @@ abstract contract TransshipmentWorker is CCIPReceiver, OwnerIsCreator, ITransshi
         _;
     }
 
-    /// @dev Updates the allowlist status of a destination chain for transactions.
-    /// @notice This function can only be called by the owner.
-    /// @param _destinationChainSelector The selector of the destination chain to be updated.
-    /// @param allowed The allowlist status to be set for the destination chain.
+    /**
+     *  @dev Updates the allowlist status of a destination chain for transactions.
+     *  @notice This function can only be called by the owner.
+     *  @param _destinationChainSelector The selector of the destination chain to be updated.
+     *  @param allowed The allowlist status to be set for the destination chain.
+     */
     function allowlistDestinationChain(uint64 _destinationChainSelector, bool allowed) external onlyOwner {
         allowlistedDestinationChains[_destinationChainSelector] = allowed;
     }
 
-    /// @dev Updates the allowlist status of a source chain
-    /// @notice This function can only be called by the owner.
-    /// @param _sourceChainSelector The selector of the source chain to be updated.
-    /// @param allowed The allowlist status to be set for the source chain.
+    /**
+     *  @dev Updates the allowlist status of a source chain
+     *  @notice This function can only be called by the owner.
+     *  @param _sourceChainSelector The selector of the source chain to be updated.
+     *  @param allowed The allowlist status to be set for the source chain.
+     */
     function allowlistSourceChain(uint64 _sourceChainSelector, bool allowed) external onlyOwner {
         allowlistedSourceChains[_sourceChainSelector] = allowed;
     }
 
-    /// @dev Updates the allowlist status of a sender for transactions.
-    /// @notice This function can only be called by the owner.
-    /// @param _sender The address of the sender to be updated.
-    /// @param allowed The allowlist status to be set for the sender.
+    /**
+     *  @dev Updates the allowlist status of a sender for transactions.
+     *  @notice This function can only be called by the owner.
+     *  @param _sender The address of the sender to be updated.
+     *  @param allowed The allowlist status to be set for the sender.
+     */
     function allowlistSender(address _sender, bool allowed) external onlyOwner {
         allowlistedSenders[_sender] = allowed;
     }
 
-    /// @notice Sends data and transfer tokens to receiver on the destination chain.
-    /// @notice Pay for fees in LINK.
-    /// @dev Assumes your contract has sufficient LINK to pay for CCIP fees.
-    /// @return messageId The ID of the CCIP message that was sent.
+    /**
+     *  @notice Sends data and transfer tokens to receiver on the destination chain.
+     *  @notice Pay for fees in LINK.
+     *  @dev Assumes your contract has sufficient LINK to pay for CCIP fees.
+     *  @return messageId The ID of the CCIP message that was sent.
+     */
     function _sendMessage(
         MassageParam calldata massageParam,
         address senderAddress
     ) internal virtual returns (bytes32 messageId);
 
-    /// @notice The entrypoint for the CCIP router to call. This function should
-    /// never revert, all errors should be handled internally in this contract.
-    /// @param any2EvmMessage The message to process.
-    /// @dev Extremely important to ensure only router calls this.
+    /**
+     *  @notice The entrypoint for the CCIP router to call. This function should
+     *  never revert, all errors should be handled internally in this contract.
+     *  @param any2EvmMessage The message to process.
+     *  @dev Extremely important to ensure only router calls this.
+     */
     function ccipReceive(
         Client.Any2EVMMessage calldata any2EvmMessage
     )
@@ -134,11 +156,13 @@ abstract contract TransshipmentWorker is CCIPReceiver, OwnerIsCreator, ITransshi
         return allKeys;
     }
 
-    /// @notice Serves as the entry point for this contract to process incoming messages.
-    /// @param any2EvmMessage Received CCIP message.
-    /// @dev Transfers specified token amounts to the owner of this contract. This function
-    /// must be external because of the  try/catch for error handling.
-    /// It uses the `onlySelf`: can only be called from the contract.
+    /**
+     *  @notice Serves as the entry point for this contract to process incoming messages.
+     *  @param any2EvmMessage Received CCIP message.
+     *  @dev Transfers specified token amounts to the owner of this contract. This function
+     *  must be external because of the  try/catch for error handling.
+     *  It uses the `onlySelf`: can only be called from the contract.
+     */
     function processMessage(
         Client.Any2EVMMessage calldata any2EvmMessage
     )
@@ -149,11 +173,12 @@ abstract contract TransshipmentWorker is CCIPReceiver, OwnerIsCreator, ITransshi
         _ccipReceive(any2EvmMessage);
     }
 
-    /// @notice Allows the owner to retry a failed message in order to unblock the associated tokens.
-    /// @param messageId The unique identifier of the failed message.
-    /// @param tokenReceiver The address to which the tokens will be sent.
-    /// @dev This function is only callable by the contract owner. It changes the status of the message
-    /// from 'failed' to 'resolved' to prevent reentry and multiple retries of the same message.
+    /** @notice Allows the owner to retry a failed message in order to unblock the associated tokens.
+     * @param messageId The unique identifier of the failed message.
+     * @param tokenReceiver The address to which the tokens will be sent.
+     * @dev This function is only callable by the contract owner. It changes the status of the message
+     * from 'failed' to 'resolved' to prevent reentry and multiple retries of the same message.
+     */
     function retryFailedMessage(bytes32 messageId, address tokenReceiver) external onlyOwner {
         if (s_failedMessages.get(messageId) != uint256(ErrorCode.BASIC)) revert MessageNotFailed(messageId);
         s_failedMessages.set(messageId, uint256(ErrorCode.RESOLVED));
@@ -162,18 +187,32 @@ abstract contract TransshipmentWorker is CCIPReceiver, OwnerIsCreator, ITransshi
         emit MessageRecovered(messageId);
     }
 
-    /// handle a received message
+    /**
+     * @dev Internal function to handle the reception of CCIP (Cross-Chain Interoperability Protocol) messages.
+     * This function is marked as virtual and must be implemented by the inheriting contract.
+     *
+     * @param any2EvmMessage The CCIP message structure containing information about the message.
+     *                      Specifically, the sender's chain, sender's address, message data, etc.
+     *                      Refer to the Client.Any2EVMMessage struct for details.
+     *
+     * Requirements:
+     * - The function must be implemented by the inheriting contract to handle the logic for processing CCIP messages.
+     * - Only addresses allowed by the `onlyAllowlisted` modifier are permitted to call this function.
+     * - The function should process the CCIP message and execute any necessary actions based on the message content.
+     */
     function _ccipReceive(Client.Any2EVMMessage memory any2EvmMessage) internal virtual override;
 
-    /// @notice Construct a CCIP message.
-    /// @dev This function will create an EVM2AnyMessage struct with all the necessary information for programmable tokens transfer.
-    /// @param _receiver The address of the receiver.
-    /// @param _data The string data to be sent.
-    /// @param _token The token to be transferred.
-    /// @param _amount The amount of the token to be transferred.
-    /// @param _feeTokenAddress The address of the token used for fees. Set address(0) for native gas.
-    /// @param _gasLimit Gas limit for destination chain.
-    /// @return Client.EVM2AnyMessage Returns an EVM2AnyMessage struct which contains information for sending a CCIP message.
+    /**
+     * @notice Construct a CCIP message.
+     * @dev This function will create an EVM2AnyMessage struct with all the necessary information for programmable tokens transfer.
+     * @param _receiver The address of the receiver.
+     * @param _data The string data to be sent.
+     * @param _token The token to be transferred.
+     * @param _amount The amount of the token to be transferred.
+     * @param _feeTokenAddress The address of the token used for fees. Set address(0) for native gas.
+     * @param _gasLimit Gas limit for destination chain.
+     * @return Client.EVM2AnyMessage Returns an EVM2AnyMessage struct which contains information for sending a CCIP message.
+     */
     function _buildCCIPMessage(
         address _receiver,
         bytes memory _data,
@@ -199,15 +238,19 @@ abstract contract TransshipmentWorker is CCIPReceiver, OwnerIsCreator, ITransshi
             });
     }
 
-    /// @notice Fallback function to allow the contract to receive Ether.
-    /// @dev This function has no function body, making it a default function for receiving Ether.
-    /// It is automatically called when Ether is sent to the contract without any data.
+    /**
+     * @notice Fallback function to allow the contract to receive Ether.
+     * @dev This function has no function body, making it a default function for receiving Ether.
+     * It is automatically called when Ether is sent to the contract without any data.
+     */
     receive() external payable {}
 
-    /// @notice Allows the contract owner to withdraw the entire balance of Ether from the contract.
-    /// @dev This function reverts if there are no funds to withdraw or if the transfer fails.
-    /// It should only be callable by the owner of the contract.
-    /// @param _beneficiary The address to which the Ether should be sent.
+    /**
+     *  @notice Allows the contract owner to withdraw the entire balance of Ether from the contract.
+     *  @dev This function reverts if there are no funds to withdraw or if the transfer fails.
+     *  It should only be callable by the owner of the contract.
+     *  @param _beneficiary The address to which the Ether should be sent.
+     */
     function withdraw(address _beneficiary) public onlyOwner {
         uint256 amount = address(this).balance;
         if (amount == 0) revert NothingToWithdraw();
@@ -215,10 +258,12 @@ abstract contract TransshipmentWorker is CCIPReceiver, OwnerIsCreator, ITransshi
         if (!sent) revert FailedToWithdrawEth(msg.sender, _beneficiary, amount);
     }
 
-    /// @notice Allows the owner of the contract to withdraw all tokens of a specific ERC20 token.
-    /// @dev This function reverts with a 'NothingToWithdraw' error if there are no tokens to withdraw.
-    /// @param _beneficiary The address to which the tokens will be sent.
-    /// @param _token The contract address of the ERC20 token to be withdrawn.
+    /**
+     * @notice Allows the owner of the contract to withdraw all tokens of a specific ERC20 token.
+     * @dev This function reverts with a 'NothingToWithdraw' error if there are no tokens to withdraw.
+     * @param _beneficiary The address to which the tokens will be sent.
+     * @param _token The contract address of the ERC20 token to be withdrawn.
+     */
     function withdrawToken(address _beneficiary, address _token) public onlyOwner {
         uint256 amount = IERC20(_token).balanceOf(address(this));
         if (amount == 0) revert NothingToWithdraw();
